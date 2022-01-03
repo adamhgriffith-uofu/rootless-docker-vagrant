1d074f9b303454562bfd83f1e316e992115e6b98
Adam Griffith <adam.h.griffith@utah.edu>
2022-01-03 16:57:14 -0700

    fixed several issues with bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,13 +11,10 @@ echo "Installing extra packages..."
 yum install git -y
 
 echo "Setting up rootless parameters in the system..."
-cat <<EOF | sudo sh -x
-
 cat <<EOT > /etc/sysctl.d/51-rootless.conf
 user.max_user_namespaces = 28633
 EOT
 sysctl --system
-EOF
 
 echo "Creating a docker user 'docky'..."
 useradd docky
@@ -28,15 +25,15 @@ echo "Adding the mappings to subuid and subgid files..."
 echo "docky:100000:65536" >> tee /etc/subuid
 echo "docky:100000:65536" >> tee /etc/subgid
 
-echo "Running docker rootless script..."
-sudo -H -u docky bash -c "curl -sSL https://get.docker.com/rootless | bash"
-
 echo "Adding env variables to bash..."
 cat <<EOF > /home/docky/.bashrc
 export XDG_RUNTIME_DIR=/home/docky/.docker/run
 export PATH=/home/docky/bin:$PATH
-export DOCKER_HOST=unix:///home/docky/.docker/run/docker.sock
+export DOCKER_HOST=unix://\${XDG_RUNTIME_DIR}/docker.sock
 EOF
+
+echo "Running docker rootless script..."
+sudo -H -u docky bash -c "curl -sSL https://get.docker.com/rootless | bash"
 
 echo "Starting docker daemon as docky user..."
 sudo -H -u docky bash -c "source /home/docky/.bashrc && /home/docky/bin/dockerd-rootless.sh"
